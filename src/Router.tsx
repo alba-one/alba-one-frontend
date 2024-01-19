85aa5854a815baf383e4a524e95d3bd254114ba8
sstaar91 <sstaar91@gmail.com>
2024-01-19 09:57:12 +0900

    default layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,24 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './components/Nav';
+import Layout from './pages/Layout';
 import List from './pages/List';
 import Detail from './pages/Detail';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import User from './pages/User';
-import Footer from './components/Footer';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
-        <Route index element={<List />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/user" element={<User />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<List />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/user" element={<User />} />
+        </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
