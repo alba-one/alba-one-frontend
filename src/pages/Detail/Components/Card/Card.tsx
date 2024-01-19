4f9a5016006fcc6cc302bdcf140141fa0c71a1b8
yeon <yeonnnnn.k@gmail.com>
2024-01-19 15:58:36 +0900

    🐣 ADD: detail page layout

--- src/pages/Detail/Components/Card/Card.tsx
+++ src/pages/Detail/Components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import Icon from 'src/components/Icon';
+import Icon from '@_icon/Icon';
 import css from './Card.module.scss';
 
 const Card = () => {
@@ -7,15 +7,24 @@ const Card = () => {
       <div className={css.storeImg} />
       <div className={css.storeInfo}>
         <div className={css.innerTitle}>시급</div>
-        <div className={css.hourlyWage}>15,000원</div>
-        <div className={css.percentBadge}>
-          기존 시급보다 50%<div className={css.arrowUp}></div>
+        <div className={css.hourlyWage}>
+          15,000원
+          <div className={css.percentBadge}>
+            기존 시급보다 50%
+            <Icon title="arrowUp" />
+          </div>
         </div>
-        <div className={css.time}>
-            <Icon 
+
+        <div className={css.when}>
+          <Icon title="clock" />
+          <div className={css.timeTable}>2023.01.19 14:00 ~ 18:00</div>
         </div>
-        <div className={css.location}></div>
-        <div className={css.greeting}></div>
+        <div className={css.where}>
+          <Icon title="location" />
+          <div className={css.location}>서울시 중구</div>
+        </div>
+        <div className={css.greeting}>편안한 분위기의 동네 카페 !</div>
+        <button className={css.applyBtn}>신청하기</button>
       </div>
     </div>
   );
--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -10,7 +10,18 @@ const Detail = () => {
           <div className={css.storeName}>지연이 카페</div>
         </div>
         <Card />
-        <div className={css.storeDescription}></div>
+        <div className={css.storeDescription}>
+          <div className={css.descriptionTitle}>공고 설명</div>
+          <div className={css.descriptionWords}>
+            이벤트를 진행하게 되어 이벤트 시간동안 일손이 부족해요! <br />
+            급하게 구하게되어 시급도 높였습니다.
+          </div>
+        </div>
+      </div>
+
+      <div className={css.showRecent}>
+        <div className={css.recentTitle}>최근에 본 공고</div>
+        <div className={css.recentGrid}></div>
       </div>
     </section>
   );
