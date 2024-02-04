458b46cf44f68a6af849aa8996537eb52ed0bc0a
sstaar91 <sstaar91@gmail.com>
2024-02-05 03:00:04 +0900

    resolve #48 ADD : 알바 - 상세 공고 조회

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,25 +1,45 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useGetUserType } from '@_hooks/useGetUserType.ts';
+
 import LargeCard from '../../components/LargeCard/LargeCard';
 import EmployerDetailBox from './Components/EmployerDetailBox/EmployerDetailBox.tsx';
 import EmployeeDetailBox from './Components/EmployeeDetailBox/EmployeeDetailBox.tsx';
 
+import { getAxios } from '@_lib/axios.ts';
+import { Notice } from '@_types/noticeTypes.ts';
 import css from './Detail.module.scss';
 
 const Detail = () => {
-  const isEmployee = false;
+  const [noticeDetail, setNoticeDetail] = useState<Notice>({});
+  const { isEmployee } = useGetUserType();
+  const params = useParams();
+
+  useEffect(() => {
+    getAxios(
+      `/shops/790cd517-d6c4-4f21-88cd-9d394d379674/notices/${params.id}`
+    ).then(({ data }) => {
+      setNoticeDetail(data.item);
+    });
+  }, []);
+
+  if (!noticeDetail.description) return null;
+
+  const { shop, ...rest } = noticeDetail;
+  const shopInfo = shop.item;
+  const notice = { ...rest };
+
   return (
     <section className={css.container}>
       <div className={css.topOfDetail}>
         <div className={css.title}>
-          <div className={css.category}>카페</div>
-          <div className={css.storeName}>지연이 카페</div>
+          <div className={css.category}>{shopInfo.category}</div>
+          <div className={css.storeName}>{shopInfo.name}</div>
         </div>
-        <LargeCard type="employee" />
+        <LargeCard isEmployee={isEmployee} notice={notice} shop={shopInfo} />
         <div className={css.storeDescription}>
           <div className={css.descriptionTitle}>공고 설명</div>
-          <div className={css.descriptionWords}>
-            이벤트를 진행하게 되어 이벤트 시간동안 일손이 부족해요! <br />
-            급하게 구하게되어 시급도 높였습니다.
-          </div>
+          <div className={css.descriptionWords}>{noticeDetail.description}</div>
         </div>
       </div>
 
