985b509e1ac48a2ec06cb5b96ae33f1eb55d8c59
sstaar91 <sstaar91@gmail.com>
2024-02-10 01:22:40 +0900

    resolve #51 ADD : 사장 - 신청자 목록 조회 및 편집

--- src/pages/Detail/Components/AnnouncedInfo/AnnouncedInfo.tsx
+++ src/pages/Detail/Components/AnnouncedInfo/AnnouncedInfo.tsx
@@ -1,16 +1,73 @@
+import { putAxios } from '@_lib/axios';
 import css from './AnnouncedInfo.module.scss';
 
-const AnnouncedInfo = () => {
+interface Props {
+  applyList: ApplyList;
+  handleRefetch: () => void;
+}
+
+interface ApplyList {
+  id: string;
+  shopId: string;
+  noticeId: string;
+  href: string;
+  item: {
+    id: string;
+    email: string;
+    type: string;
+    name: string;
+    phone: string;
+    address: string;
+    bio: string;
+  };
+  status: string;
+}
+
+const AnnouncedInfo = ({ applyList, handleRefetch }: Props) => {
+  const { name, bio, phone } = applyList.item;
+  const { shopId, noticeId, id } = applyList;
+
+  const handleCancleApply = (status: string) => {
+    putAxios(`/shops/${shopId}/notices/${noticeId}/applications/${id}`, {
+      status: status,
+    }).then(() => handleRefetch());
+  };
+
   return (
     <div className={css.announcedInfo}>
-      <div className={css.columnItems}>name</div>
-      <div className={css.columnItems}>description</div>
-      <div className={css.columnItems}>contact</div>
+      <div className={css.columnItems}>{name}</div>
+      <div className={css.columnItems}>{bio}</div>
+      <div className={css.columnItems}>{phone}</div>
       <div className={css.columnItems}>
-        <span className={css.badge}>status</span>
+        {applyList.status === 'pending' ? (
+          <div className={css.buttonBox}>
+            <button
+              className={css.negativeBtn}
+              onClick={() => handleCancleApply('rejected')}
+            >
+              거절하기
+            </button>
+            <button
+              className={css.positiveBtn}
+              onClick={() => handleCancleApply('accepted')}
+            >
+              승인하기
+            </button>
+          </div>
+        ) : (
+          <span className={`${css.badge} ${css[applyList.status]}`}>
+            {BADGE_TITLE[applyList.status]}
+          </span>
+        )}
       </div>
     </div>
   );
 };
 
 export default AnnouncedInfo;
+
+const BADGE_TITLE: { [key: string]: string } = {
+  accepted: '승인 완료',
+  rejected: '거절',
+  canceled: '취소',
+};
--- src/pages/Detail/Components/EmployerDetailBox/EmployerDetailBox.tsx
+++ src/pages/Detail/Components/EmployerDetailBox/EmployerDetailBox.tsx
@@ -2,14 +2,41 @@ import Icon from '@_components/Icon';
 import AnnouncedInfo from '../AnnouncedInfo/AnnouncedInfo.tsx';
 
 import css from './EmployerDetailBox.module.scss';
+import { useEffect, useState } from 'react';
+import { getAxios } from '@_lib/axios.ts';
+import { useParams } from 'react-router-dom';
 
 const EmployerDetailBox = () => {
+  const [applyUserList, setApplyUserList] = useState([]);
+  const [refetch, setRefetch] = useState(false);
+  const handleRefetch = () => {
+    setRefetch(prev => !prev);
+  };
+
   let total = 30;
   let limit = 5;
 
   let pageNum = total / limit;
 
   let pageNumArr = new Array(pageNum).fill(1);
+
+  const params = useParams();
+  useEffect(() => {
+    getAxios(`/shops/c9412e6d-a49d-4e19-9144-5f0dc958db9f/notices/${params.id}/applications
+    `).then(res => {
+      setApplyUserList(
+        res.data.items.map((el: any) => ({
+          ...el.item.user,
+          id: el.item.id,
+          shopId: el.item.shop.item.id,
+          noticeId: el.item.notice.item.id,
+          status: el.item.status,
+        }))
+      );
+    });
+  }, [refetch]);
+
+  console.log(applyUserList);
 
   return (
     <section className={css.annouceBox}>
@@ -19,13 +46,15 @@ const EmployerDetailBox = () => {
         <div className={css.titleColumn}>전화번호</div>
         <div className={css.titleColumn}>상태</div>
       </div>
-
-      <AnnouncedInfo />
-      <AnnouncedInfo />
-      <AnnouncedInfo />
-      <AnnouncedInfo />
-      <AnnouncedInfo />
-      <AnnouncedInfo />
+      {applyUserList.map((list, idx) => {
+        return (
+          <AnnouncedInfo
+            key={list?.item.id + idx}
+            applyList={list}
+            handleRefetch={handleRefetch}
+          />
+        );
+      })}
 
       <div className={`${css.announceInfo} ${css.pagination}`}>
         <div className={css.pageNum}>
